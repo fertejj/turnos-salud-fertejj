60d7ae86b5802cbdc6d8116dc216a12f6eb4ea07
Fer Tejerina <fertejerina97@gmail.com>
2025-04-06 12:40:39 -0300

    Modifica props AddTimeSlot agrega "day"

--- src/pages/dashboard/professional/AddTimeSlot.tsx
+++ src/pages/dashboard/professional/AddTimeSlot.tsx
@@ -3,6 +3,7 @@ import { FiCheck } from "react-icons/fi";
 type Props = {
   from: string;
   to: string;
+  day: any;
   onChangeFrom: (val: string) => void;
   onChangeTo: (val: string) => void;
   onConfirm: () => void;
@@ -16,6 +17,7 @@ export default function AddTimeSlot({
   onChangeFrom,
   onChangeTo,
   onConfirm,
+  day,
   isValid,
   validationError,
 }: Props) {
